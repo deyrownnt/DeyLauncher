01db56ca9f8c5ec359e2e1488db37b91423cdf25
deyrownnt <deyrownnt@gmail.com>
2026-09-25 22:06:48 +0300

    chore: remove stray io image artifact and unused ui-harness scaffolding

--- ui-harness/shim/src/org/junit/jupiter/api/io/TempDir.java
+++
@@ -1,4 +0,0 @@
-package org.junit.jupiter.api.io;
-import java.lang.annotation.*;
-@Retention(RetentionPolicy.RUNTIME) @Target({ElementType.PARAMETER, ElementType.FIELD})
-public @interface TempDir {}
